e0143e9357ced6c1e01f32ea20a7024921b06db3
lsfernandes92 <lsfernandes92@gmail.com>
2019-07-16 17:58:13 -0300

    Add the ability to auto login after sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      login @user
       flash[:success] = "Welcome to the Sample app!"
       redirect_to @user
     else
