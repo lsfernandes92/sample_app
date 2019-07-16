8968525f4482795eb60e4715ed3c0381eb643b4a
lsfernandes92 <lsfernandes92@gmail.com>
2019-07-16 18:19:06 -0300

    Enable logout option

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
