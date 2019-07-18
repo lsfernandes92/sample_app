4a2d79b07d98ae03e4c8951af14493e8445a63d8
lsfernandes92 <lsfernandes92@gmail.com>
2019-07-18 16:32:26 -0300

    Fix given access to update user info only to logged user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :logged_in_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -37,5 +38,12 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = 'Please log in first'
+        redirect_to login_url
+      end
     end
 end
