f90237b4931359d8906b14b1d9228ef1fc285a5c
lsfernandes92 <lsfernandes92@gmail.com>
2019-07-18 20:08:54 -0300

    Add migration to add admin option to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_224952) do
+ActiveRecord::Schema.define(version: 2019_07_18_225614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_224952) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_disgest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
