d88de30cf9290ba2cc7a8ec0a26ebbe6c774ebea
lsfernandes92 <lsfernandes92@gmail.com>
2019-07-22 18:13:38 -0300

    Add virtual activation token attribute. Also refactory email downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  before_save { email.downcase! }
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name,
     presence: true,
@@ -48,4 +49,15 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_disgest, nil)
   end
+
+  private
+
+    def downcase_email
+      self.email = email.downcase
+    end
+
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
