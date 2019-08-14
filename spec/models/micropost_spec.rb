402f34531236eb85d7264426b5c300b5e18b8c74
lsfernandes92 <lsfernandes92@gmail.com>
2019-08-14 16:20:57 -0700

    Add default scope to bring the most recent psot

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -14,12 +14,12 @@ RSpec.describe Micropost, type: :model do
   end
 
   let(:user) { User.first }
-  
+
   let(:micropost) do
     user.microposts.build(content: 'Lorem Ipsum', user_id: User.first.id)
   end
 
-  context "with validations" do
+  context 'with validations' do
     it { expect(micropost).to be_valid }
 
     it 'user id should be present' do
@@ -35,6 +35,28 @@ RSpec.describe Micropost, type: :model do
     it 'content should be at most 140 characters' do
       micropost.content = 'a' * 141
       expect(micropost).not_to be_valid
+    end
+  end
+
+  context 'show most recent post created' do
+    before do
+      Micropost.create(
+        content: 'foo bar',
+        user_id: user.id,
+        created_at: Time.zone.now
+      )
+
+      Micropost.create(
+        content: 'go to bar',
+        user_id: user.id,
+        created_at: Time.zone.now + 5.minutes
+      )
+    end
+
+    let(:most_recent_post) { Micropost.first }
+
+    it 'should return the most recent post created' do
+      expect(most_recent_post.content).to eq 'go to bar'
     end
   end
 end
