1609e2518957f4fac95afefc04b7fdd27dc32b3d
lsfernandes92 <lsfernandes92@gmail.com>
2019-08-15 19:45:50 -0700

    Permit post new microposts when user logged in throught home page

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,10 +2,22 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :delete]
 
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash['success'] = 'Micropost created!'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
 
   end
 
-  def delete
+  private
 
-  end
+    def micropost_params()
+      params.require(:micropost).permit(:content)
+    end
 end
