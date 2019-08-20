b7b959faf046583b06fc82d1805bf3c5c4099dbe
lsfernandes92 <lsfernandes92@gmail.com>
2019-08-19 19:02:45 -0700

    Implement image resize and upload file size and type restriction

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,15 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate :picture_size
 
   default_scope -> { order 'created_at DESC' }
+
+  private
+
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, 'should be less than 5MB')
+      end
+    end
 end
